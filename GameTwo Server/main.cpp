2b8c8040c3dfed2f3eb5b2c8046a074aed937408
Ap. Jatrid <warjo@Ap-Jatrids-MacBook.local>
2013-06-08 15:08:58 +0300

    Initial Commit

---
+++ GameTwo Server/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GameTwo Server
+//
+//  Created by warjo on 6/8/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
